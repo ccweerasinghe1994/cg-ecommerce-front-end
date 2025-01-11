f195e67b7f71a7c997b1d1662cfa99a3bf616fdd
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-01-11 11:22:04 +0530

    adding tansstack deps

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,19 @@ import App from "./App.tsx";
 import { ErrorBoundary } from "react-error-boundary";
 import Fallback from "./errors/errorFallback.tsx";
 import { Toaster } from "@/components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ErrorBoundary FallbackComponent={Fallback}>
-      <App />
-      <Toaster richColors position="top-center" />
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary FallbackComponent={Fallback}>
+        <App />
+        <Toaster richColors position="top-center" />
+      </ErrorBoundary>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
